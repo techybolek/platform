8918a896cb7756beeec4ef31081f3f51ea1df36a
supunm <supunm@a5903396-d722-0410-b921-86c7d4935375>
2013-02-18 04:28:45 +0000

    applying patch from WSAS-1108
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@162189 a5903396-d722-0410-b921-86c7d4935375

--- components/wsdl2code/org.wso2.carbon.modules.wsdl2code/src/main/java/org/wso2/carbon/wsdl2code/WSDL2Code.java
+++ components/wsdl2code/org.wso2.carbon.modules.wsdl2code/src/main/java/org/wso2/carbon/wsdl2code/WSDL2Code.java
@@ -16,12 +16,6 @@
 
 package org.wso2.carbon.wsdl2code;
 
-import org.apache.axiom.om.OMAbstractFactory;
-import org.apache.axiom.om.OMAttribute;
-import org.apache.axiom.om.OMElement;
-import org.apache.axiom.om.OMFactory;
-import org.apache.axiom.om.impl.builder.StAXOMBuilder;
-import org.apache.axiom.om.xpath.AXIOMXPath;
 import org.apache.axis2.AxisFault;
 import org.apache.axis2.addressing.EndpointReference;
 import org.apache.axis2.context.ConfigurationContext;
@@ -30,37 +24,19 @@ import org.apache.axis2.engine.AxisConfiguration;
 import org.apache.axis2.util.CommandLineOption;
 import org.apache.axis2.util.CommandLineOptionConstants;
 import org.apache.axis2.util.CommandLineOptionParser;
-import org.apache.axis2.wsdl.codegen.CodeGenerationEngine;
 import org.apache.axis2.wsdl.util.WSDL2JavaOptionsValidator;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.jaxen.JaxenException;
-import org.jaxen.XPath;
 import org.wso2.carbon.core.AbstractAdmin;
 import org.wso2.carbon.core.transports.http.HttpTransportListener;
-import org.wso2.carbon.utils.*;
-
+import org.wso2.carbon.utils.ArchiveManipulator;
+import org.wso2.carbon.utils.FileManipulator;
+import org.wso2.carbon.utils.NetworkUtils;
+import org.wso2.carbon.utils.ServerConstants;
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
-import javax.xml.namespace.QName;
-import javax.xml.stream.XMLInputFactory;
-import javax.xml.stream.XMLStreamException;
-import javax.xml.stream.XMLStreamReader;
-import javax.xml.transform.Result;
-import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.net.SocketException;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -109,8 +85,8 @@ public class WSDL2Code extends AbstractAdmin {
             String option = options[j];
             optionsList.add(option);
         }
-        optionsList.add("-o");
-        optionsList.add(codegenOutputDir);
+
+
         String[] args = optionsList.toArray(new String[optionsList.size()]);
         Map allOptions;
         try {
@@ -120,12 +96,12 @@ public class WSDL2Code extends AbstractAdmin {
             List list = commandLineOptionParser.getInvalidOptions(new WSDL2JavaOptionsValidator());
             if (list.size() > 0) {
                 String faultOptions = "";
-                for (Iterator iterator = list.iterator(); iterator.hasNext();) {
+                for (Iterator iterator = list.iterator(); iterator.hasNext(); ) {
                     CommandLineOption commandLineOption = (CommandLineOption) iterator.next();
                     String optionValue = commandLineOption.getOptionValue();
                     faultOptions += "Invalid input for [ " + commandLineOption.getOptionType() +
-                                    (optionValue != null ? " : " + optionValue + " ]" : " ]") +
-                                    "\n";
+                            (optionValue != null ? " : " + optionValue + " ]" : " ]") +
+                            "\n";
                 }
 
                 log.error(faultOptions);
@@ -144,7 +120,8 @@ public class WSDL2Code extends AbstractAdmin {
                     throw new AxisFault("The wsdl uri should be a URL or a valid path on the file system");
                 }
             }
-            new CodeGenerationEngine(commandLineOptionParser).generate();
+//            new CodeGenerationEngine(commandLineOptionParser).generate();
+            (new POMGenerator()).generateAxis2Client(allOptions, codegenOutputDir);
         } catch (Exception e) {
             String rootMsg = "Code generation failed";
             Throwable throwable = e.getCause();
@@ -175,58 +152,7 @@ public class WSDL2Code extends AbstractAdmin {
             }
             String destFileName = uuid.substring(2) + ".zip";
             String destArchive = destDir.getAbsolutePath() + File.separator + destFileName;
-            InputStream pomXslInputStream = getClass().getResourceAsStream(CODEGEN_POM_XSL);
-            if (pomXslInputStream == null) {
-                pomXslInputStream = Thread.currentThread().getContextClassLoader()
-                        .getResourceAsStream(CODEGEN_POM_XSL);
-            }
 
-            String name = uuid;
-            boolean isBuildXml = false;
-            String version = "1.0";
-            File buildXml = new File(codegenOutputDir, "build.xml");
-            if (buildXml.exists() && buildXml.isFile()) {
-                isBuildXml = true;
-                InputStream buildInputStream = new FileInputStream(buildXml);
-                XMLStreamReader streamReader =
-                        XMLInputFactory.newInstance().createXMLStreamReader(buildInputStream);
-                StAXOMBuilder builder = new StAXOMBuilder(streamReader);
-                XPath xp = new AXIOMXPath("/project/property[@name='name']");
-                OMElement documentElement = builder.getDocumentElement();
-                OMElement nameEle = (OMElement) xp.selectSingleNode(documentElement);
-                if (nameEle != null) {
-                    OMAttribute omAttribute = nameEle.getAttribute(new QName("value"));
-                    String nameVal = omAttribute.getAttributeValue();
-                    if (nameVal != null) {
-                        name = nameVal;
-                    }
-                }
-            }
-            OMFactory fac = OMAbstractFactory.getOMFactory();
-            OMElement infoEle = fac.createOMElement(new QName("info"));
-            OMElement nameEle = fac.createOMElement(new QName("name"));
-            nameEle.setText(formatServiceName(name));
-            infoEle.addChild(nameEle);
-            OMElement isBuildXmlEle = fac.createOMElement(new QName("isBuildXml"));
-            isBuildXmlEle.setText(Boolean.valueOf(isBuildXml).toString());
-            infoEle.addChild(isBuildXmlEle);
-            OMElement versionEle = fac.createOMElement(new QName("version"));
-            versionEle.setText(version);
-            infoEle.addChild(versionEle);
-            ByteArrayOutputStream bao = new ByteArrayOutputStream();
-            infoEle.serialize(bao);
-            InputStream xmlInputStream = new ByteArrayInputStream(bao.toByteArray());
-
-            if (pomXslInputStream != null) {
-                File pomFileOut = new File(codegenOutputDir, "pom.xml");
-                FileOutputStream pomFileOutputStream = new FileOutputStream(pomFileOut);
-                Source xmlSource = new StreamSource(xmlInputStream);
-                Source xslSource = new StreamSource(pomXslInputStream);
-                Result result = new StreamResult(pomFileOutputStream);
-                Transformer transformer =
-                        TransformerFactory.newInstance().newTransformer(xslSource);
-                transformer.transform(xmlSource, result);
-            }
 
             new ArchiveManipulator().archiveDir(destArchive, new File(codegenOutputDir).getPath());
             FileManipulator.deleteDir(new File(codegenOutputDir));
@@ -249,21 +175,160 @@ public class WSDL2Code extends AbstractAdmin {
             String msg = WSDL2Code.class.getName() + " IOException has occured.";
             log.error(msg, e);
             throw new AxisFault(msg, e);
-        } catch (XMLStreamException e) {
-            String msg =
-                    WSDL2Code.class.getName() + " error encountred while reading the build.xml";
+        }
+    }
+
+    /**
+     * Generates code for CXF
+     *
+     * @param options
+     * @return
+     * @throws AxisFault
+     */
+    public CodegenDownloadData codegenForCXF(String[] options) throws AxisFault {
+        boolean isJaxWs = true; //Otherwise JaxRS
+        String uuid = String.valueOf(System.currentTimeMillis() + Math.random());
+        ConfigurationContext configContext = getConfigContext();
+        String codegenOutputDir = configContext.getProperty(ServerConstants.WORK_DIR) + File.separator +
+                "tools_codegen" + File.separator + uuid + File.separator;
+        System.getProperties().remove("project.base.dir");
+        System.getProperties().remove("name");
+        System.setProperty("project.base.dir", codegenOutputDir);
+
+        ArrayList<String> optionsList = new ArrayList<String>();
+        for (String s : options) {
+            if (s.equalsIgnoreCase("-jaxws") || s.equalsIgnoreCase("-jaxrs")) {
+                if (s.equals("-jaxrs")) {
+                    isJaxWs = false;
+                    //This is only to distinguish jaxws and jaxrs. Therefore no need of adding this to options list
+                    continue;
+                }
+            }
+            optionsList.add(s);
+        }
+
+        if (isJaxWs) {
+            return getJaxWSCodegenDownloadData(configContext, codegenOutputDir, optionsList);
+        } else {
+            return getJaxRSCodegenDownloadData(configContext, codegenOutputDir, optionsList);
+        }
+    }
+
+    private CodegenDownloadData getJaxWSCodegenDownloadData(ConfigurationContext configContext, String codegenOutputDir, ArrayList<String> optionsList) throws AxisFault {
+        String uuid;
+        optionsList.add("-frontend");
+        optionsList.add("jaxws21");
+        optionsList.add("-client");
+
+
+        String[] args = optionsList.toArray(new String[optionsList.size()]);
+
+        try {
+            (new POMGenerator()).generateJaxWSClient(optionsList, codegenOutputDir);
+        } catch (Exception e) {
+            String rootMsg = "Code generation failed";
+            Throwable throwable = e.getCause();
+            if (throwable != null) {
+                String msg = throwable.getMessage();
+                if (msg != null) {
+                    log.error(rootMsg + " " + msg, throwable);
+                    throw new AxisFault(throwable.toString());
+                }
+            }
+            log.error(rootMsg, e);
+            throw AxisFault.makeFault(e);
+        }
+
+        try {
+            //achive destination
+            uuid = String.valueOf(System.currentTimeMillis() + Math.random());
+            File destDir = new File(configContext.getProperty(ServerConstants.WORK_DIR) + File.separator +
+                    "tools_codegen" +
+                    File.separator +
+                    uuid);
+            if (!destDir.exists()) {
+                destDir.mkdirs();
+            }
+            String destFileName = uuid.substring(2) + ".zip";
+            String destArchive = destDir.getAbsolutePath() + File.separator + destFileName;
+
+            new ArchiveManipulator().archiveDir(destArchive, new File(codegenOutputDir).getPath());
+            FileManipulator.deleteDir(new File(codegenOutputDir));
+
+            DataHandler handler;
+            if (destArchive != null) {
+                File file = new File(destArchive);
+                FileDataSource datasource = new FileDataSource(file);
+                handler = new DataHandler(datasource);
+
+                CodegenDownloadData data = new CodegenDownloadData();
+                data.setFileName(file.getName());
+                data.setCodegenFileData(handler);
+                return data;
+            } else {
+                return null;
+            }
+
+        } catch (IOException e) {
+            String msg = WSDL2Code.class.getName() + " IOException has occured.";
             log.error(msg, e);
             throw new AxisFault(msg, e);
-        } catch (JaxenException e) {
-            String msg = WSDL2Code.class.getName() + " xpath error has occured";
-            log.error(msg, e);
-            throw new AxisFault(msg, e);
-        } catch (TransformerConfigurationException e) {
-            String msg = WSDL2Code.class.getName() + " transformation error has occured";
-            log.error(msg, e);
-            throw new AxisFault(msg, e);
-        } catch (TransformerException e) {
-            String msg = WSDL2Code.class.getName() + " transformation error has occured";
+        }
+    }
+
+    private CodegenDownloadData getJaxRSCodegenDownloadData(ConfigurationContext configContext, String codegenOutputDir, ArrayList<String> optionsList) throws AxisFault {
+        String uuid;
+
+        String[] args = optionsList.toArray(new String[optionsList.size()]);
+
+        try {
+            POMGenerator.generateJaxRSClient(optionsList, codegenOutputDir);
+        } catch (Exception e) {
+            String rootMsg = "Code generation failed";
+            Throwable throwable = e.getCause();
+            if (throwable != null) {
+                String msg = throwable.getMessage();
+                if (msg != null) {
+                    log.error(rootMsg + " " + msg, throwable);
+                    throw new AxisFault(throwable.toString());
+                }
+            }
+            log.error(rootMsg, e);
+            throw AxisFault.makeFault(e);
+        }
+
+        try {
+            //achive destination
+            uuid = String.valueOf(System.currentTimeMillis() + Math.random());
+            File destDir = new File(configContext.getProperty(ServerConstants.WORK_DIR) + File.separator +
+                    "tools_codegen" +
+                    File.separator +
+                    uuid);
+            if (!destDir.exists()) {
+                destDir.mkdirs();
+            }
+            String destFileName = uuid.substring(2) + ".zip";
+            String destArchive = destDir.getAbsolutePath() + File.separator + destFileName;
+
+            new ArchiveManipulator().archiveDir(destArchive, new File(codegenOutputDir).getPath());
+            FileManipulator.deleteDir(new File(codegenOutputDir));
+
+            DataHandler handler;
+            if (destArchive != null) {
+                File file = new File(destArchive);
+                FileDataSource datasource = new FileDataSource(file);
+                handler = new DataHandler(datasource);
+
+                CodegenDownloadData data = new CodegenDownloadData();
+                data.setFileName(file.getName());
+                data.setCodegenFileData(handler);
+                return data;
+            } else {
+                return null;
+            }
+
+        } catch (IOException e) {
+            String msg = WSDL2Code.class.getName() + " IOException has occured.";
             log.error(msg, e);
             throw new AxisFault(msg, e);
         }
