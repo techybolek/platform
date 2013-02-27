666e5f2dc633301ed650c813607ed911b5d4123b
kasung@wso2.com <kasung@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-02-27 20:05:47 +0000

    deleting governance 408 changes
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@165703 a5903396-d722-0410-b921-86c7d4935375

--- components/governance/org.wso2.carbon.governance.notifications.ui/src/main/resources/web/notifications/js/paginate.js
+++
@@ -1,7 +0,0 @@
-function submitSubscription(page, pageNumber){
-    sessionAwareFunction(function(){
-        location.href="notifications.jsp?region=region1&item=governance_notification_menu&requestedPage="+pageNumber;
-
-    }, org_wso2_carbon_governance_notifications_ui_jsi18n["session.timed.out"])
-
-}
