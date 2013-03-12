df13b01e7481ee8d4db472d6ccca3c24c1135b0a
isuruw@wso2.com <isuruw@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-03-12 06:44:13 +0000

    migrating Stratos 2 new dependencies,service-stubs,components and features from scratch workspace to trunk
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@167513 a5903396-d722-0410-b921-86c7d4935375

--- components/hosting-mgt/org.wso2.carbon.hosting.mgt/src/main/java/org/wso2/carbon/hosting/mgt/utils/CartridgeConstants.java
+++
@@ -1,8 +0,0 @@
-package org.wso2.carbon.hosting.mgt.utils;
-
-public class CartridgeConstants {
-
-    public static String CARTRIDGE_TITLES= "cartridge.titles";
-    public static String AUTOSCALER_SERVICE_URL= "autoscalerService.url";
-
-}
