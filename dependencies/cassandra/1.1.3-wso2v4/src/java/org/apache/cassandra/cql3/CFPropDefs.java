5d63b08de46c3670af3bb01a0d147674631cc8f5
sinthuja@wso2.com <sinthuja@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-06-18 10:53:44 +0000

    adding cassandra v4
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@175474 a5903396-d722-0410-b921-86c7d4935375

---
+++ dependencies/cassandra/1.1.3-wso2v4/src/java/org/apache/cassandra/cql3/CFPropDefs.java
@@ -0,0 +1,258 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.cassandra.cql3;
+
+import com.google.common.collect.Sets;
+import org.apache.cassandra.config.CFMetaData;
+import org.apache.cassandra.config.ConfigurationException;
+import org.apache.cassandra.db.marshal.AbstractType;
+import org.apache.cassandra.db.marshal.TypeParser;
+import org.apache.cassandra.io.compress.CompressionParameters;
+import org.apache.cassandra.io.compress.SnappyCompressor;
+import org.apache.cassandra.thrift.InvalidRequestException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class CFPropDefs
+{
+    private static Logger logger = LoggerFactory.getLogger(CFPropDefs.class);
+
+    public static final String KW_COMMENT = "comment";
+    public static final String KW_READREPAIRCHANCE = "read_repair_chance";
+    public static final String KW_DCLOCALREADREPAIRCHANCE = "dclocal_read_repair_chance";
+    public static final String KW_GCGRACESECONDS = "gc_grace_seconds";
+    public static final String KW_MINCOMPACTIONTHRESHOLD = "min_compaction_threshold";
+    public static final String KW_MAXCOMPACTIONTHRESHOLD = "max_compaction_threshold";
+    public static final String KW_REPLICATEONWRITE = "replicate_on_write";
+    public static final String KW_COMPACTION_STRATEGY_CLASS = "compaction_strategy_class";
+    public static final String KW_CACHING = "caching";
+    public static final String KW_BF_FP_CHANCE = "bloom_filter_fp_chance";
+
+    // Maps CQL short names to the respective Cassandra comparator/validator class names
+    public static final Map<String, String> comparators = new HashMap<String, String>();
+    public static final Set<String> keywords = new HashSet<String>();
+    public static final Set<String> obsoleteKeywords = new HashSet<String>();
+    public static final Set<String> allowedKeywords = new HashSet<String>();
+
+    public static final String COMPACTION_OPTIONS_PREFIX = "compaction_strategy_options";
+    public static final String COMPRESSION_PARAMETERS_PREFIX = "compression_parameters";
+
+    static
+    {
+        comparators.put("ascii", "AsciiType");
+        comparators.put("bigint", "LongType");
+        comparators.put("blob", "BytesType");
+        comparators.put("boolean", "BooleanType");
+        comparators.put("counter", "CounterColumnType");
+        comparators.put("decimal", "DecimalType");
+        comparators.put("double", "DoubleType");
+        comparators.put("float", "FloatType");
+        comparators.put("int", "Int32Type");
+        comparators.put("text", "UTF8Type");
+        comparators.put("timestamp", "DateType");
+        comparators.put("uuid", "UUIDType");
+        comparators.put("varchar", "UTF8Type");
+        comparators.put("varint", "IntegerType");
+        comparators.put("timeuuid", "TimeUUIDType");
+
+        keywords.add(KW_COMMENT);
+        keywords.add(KW_READREPAIRCHANCE);
+        keywords.add(KW_DCLOCALREADREPAIRCHANCE);
+        keywords.add(KW_GCGRACESECONDS);
+        keywords.add(KW_REPLICATEONWRITE);
+        keywords.add(KW_COMPACTION_STRATEGY_CLASS);
+        keywords.add(KW_CACHING);
+        keywords.add(KW_BF_FP_CHANCE);
+
+        allowedKeywords.addAll(keywords);
+        allowedKeywords.addAll(obsoleteKeywords);
+    }
+
+    public final Map<String, String> properties = new HashMap<String, String>();
+    public final Map<String, String> compactionStrategyOptions = new HashMap<String, String>();
+    public final Map<String, String> compressionParameters = new HashMap<String, String>()
+    {{
+        if (CFMetaData.DEFAULT_COMPRESSOR != null)
+            put(CompressionParameters.SSTABLE_COMPRESSION, CFMetaData.DEFAULT_COMPRESSOR);
+    }};
+
+    public static AbstractType<?> parseType(String type) throws InvalidRequestException
+    {
+        try
+        {
+            String className = comparators.get(type);
+            if (className == null)
+                className = type;
+            return TypeParser.parse(className);
+        }
+        catch (ConfigurationException e)
+        {
+            InvalidRequestException ex = new InvalidRequestException(e.toString());
+            ex.initCause(e);
+            throw ex;
+        }
+    }
+
+    /* If not comparator/validator is not specified, default to text (BytesType is the wrong default for CQL
+     * since it uses hex terms).  If the value specified is not found in the comparators map, assume the user
+     * knows what they are doing (a custom comparator/validator for example), and pass it on as-is.
+     */
+
+    public void validate() throws ConfigurationException
+    {
+        // Catch the case where someone passed a kwarg that is not recognized.
+        for (String bogus : Sets.difference(properties.keySet(), allowedKeywords))
+            throw new ConfigurationException(bogus + " is not a valid keyword argument for CREATE TABLE");
+        for (String obsolete : Sets.intersection(properties.keySet(), obsoleteKeywords))
+            logger.warn("Ignoring obsolete property {}", obsolete);
+    }
+
+    /** Map a keyword to the corresponding value */
+    public void addProperty(String name, String value)
+    {
+        String[] composite = name.split(":");
+        if (composite.length > 1)
+        {
+            if (composite[0].equals(COMPACTION_OPTIONS_PREFIX))
+            {
+                compactionStrategyOptions.put(composite[1], value);
+                return;
+            }
+            else if (composite[0].equals(COMPRESSION_PARAMETERS_PREFIX))
+            {
+                compressionParameters.put(composite[1], value);
+                return;
+            }
+        }
+        properties.put(name, value);
+    }
+
+    public void addAll(Map<String, String> propertyMap)
+    {
+        for (Map.Entry<String, String> entry : propertyMap.entrySet())
+            addProperty(entry.getKey(), entry.getValue());
+    }
+
+    public Boolean hasProperty(String name)
+    {
+        return properties.containsKey(name);
+    }
+
+    public void applyToCFMetadata(CFMetaData cfm) throws ConfigurationException
+    {
+        if (hasProperty(KW_COMMENT))
+            cfm.comment(get(KW_COMMENT));
+
+        cfm.readRepairChance(getDouble(KW_READREPAIRCHANCE, cfm.getReadRepairChance()));
+        cfm.dcLocalReadRepairChance(getDouble(KW_DCLOCALREADREPAIRCHANCE, cfm.getDcLocalReadRepair()));
+        cfm.gcGraceSeconds(getInt(KW_GCGRACESECONDS, cfm.getGcGraceSeconds()));
+        cfm.replicateOnWrite(getBoolean(KW_REPLICATEONWRITE, cfm.getReplicateOnWrite()));
+        cfm.minCompactionThreshold(toInt(KW_MINCOMPACTIONTHRESHOLD, compactionStrategyOptions.get(KW_MINCOMPACTIONTHRESHOLD), cfm.getMinCompactionThreshold()));
+        cfm.maxCompactionThreshold(toInt(KW_MAXCOMPACTIONTHRESHOLD, compactionStrategyOptions.get(KW_MAXCOMPACTIONTHRESHOLD), cfm.getMaxCompactionThreshold()));
+        cfm.caching(CFMetaData.Caching.fromString(getString(KW_CACHING, cfm.getCaching().toString())));
+        cfm.bloomFilterFpChance(getDouble(KW_BF_FP_CHANCE, cfm.getBloomFilterFpChance()));
+
+        if (!compactionStrategyOptions.isEmpty())
+            cfm.compactionStrategyOptions(new HashMap<String, String>(compactionStrategyOptions));
+
+        if (!compressionParameters.isEmpty())
+            cfm.compressionParameters(CompressionParameters.create(compressionParameters));
+    }
+
+    public String get(String name)
+    {
+        return properties.get(name);
+    }
+
+    public String getString(String key, String defaultValue)
+    {
+        String value = properties.get(key);
+        return value != null ? value : defaultValue;
+    }
+
+    // Return a property value, typed as a Boolean
+    public Boolean getBoolean(String key, Boolean defaultValue)
+    {
+        String value = properties.get(key);
+        return (value == null) ? defaultValue : value.toLowerCase().matches("(1|true|yes)");
+    }
+
+    // Return a property value, typed as a Double
+    public Double getDouble(String key, Double defaultValue) throws ConfigurationException
+    {
+        Double result;
+        String value = properties.get(key);
+
+        if (value == null)
+            result = defaultValue;
+        else
+        {
+            try
+            {
+                result = Double.parseDouble(value);
+            }
+            catch (NumberFormatException e)
+            {
+                throw new ConfigurationException(String.format("%s not valid for \"%s\"", value, key));
+            }
+        }
+        return result;
+    }
+
+    // Return a property value, typed as an Integer
+    public Integer getInt(String key, Integer defaultValue) throws ConfigurationException
+    {
+        String value = properties.get(key);
+        return toInt(key, value, defaultValue);
+    }
+
+    public static Integer toInt(String key, String value, Integer defaultValue) throws ConfigurationException
+    {
+        Integer result;
+
+        if (value == null)
+            result = defaultValue;
+        else
+        {
+            try
+            {
+                result = Integer.parseInt(value);
+            }
+            catch (NumberFormatException e)
+            {
+                throw new ConfigurationException(String.format("%s not valid for \"%s\"", value, key));
+            }
+        }
+        return result;
+    }
+
+    @Override
+    public String toString()
+    {
+        return String.format("CFPropDefs(%s, compaction: %s, compression: %s)",
+                             properties.toString(),
+                             compactionStrategyOptions.toString(),
+                             compressionParameters.toString());
+    }
+}
---
+++ dependencies/cassandra/1.1.3-wso2v4/src/java/org/apache/cassandra/db/compaction/SizeTieredCompactionStrategy.java
@@ -0,0 +1,211 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.cassandra.db.compaction;
+
+import java.util.*;
+import java.util.Map.Entry;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.cassandra.db.ColumnFamilyStore;
+import org.apache.cassandra.io.sstable.SSTable;
+import org.apache.cassandra.io.sstable.SSTableReader;
+import org.apache.cassandra.utils.Pair;
+
+public class SizeTieredCompactionStrategy extends AbstractCompactionStrategy
+{
+    private static final Logger logger = LoggerFactory.getLogger(SizeTieredCompactionStrategy.class);
+    protected static final long DEFAULT_MIN_SSTABLE_SIZE = 50L * 1024L * 1024L;
+    protected static final String MIN_SSTABLE_SIZE_KEY = "min_sstable_size";
+    protected long minSSTableSize;
+    protected volatile int estimatedRemainingTasks;
+
+    public SizeTieredCompactionStrategy(ColumnFamilyStore cfs, Map<String, String> options)
+    {
+        super(cfs, options);
+        this.estimatedRemainingTasks = 0;
+        String optionValue = options.get(MIN_SSTABLE_SIZE_KEY);
+        minSSTableSize = (null != optionValue) ? Long.parseLong(optionValue) : DEFAULT_MIN_SSTABLE_SIZE;
+        cfs.setCompactionThresholds(cfs.metadata.getMinCompactionThreshold(), cfs.metadata.getMaxCompactionThreshold());
+    }
+
+    public AbstractCompactionTask getNextBackgroundTask(final int gcBefore)
+    {
+        if (cfs.isCompactionDisabled())
+        {
+            logger.debug("Compaction is currently disabled.");
+            return null;
+        }
+
+        Set<SSTableReader> candidates = cfs.getUncompactingSSTables();
+        List<List<SSTableReader>> buckets = getBuckets(createSSTableAndLengthPairs(filterSuspectSSTables(candidates)), minSSTableSize);
+        logger.debug("Compaction buckets are {}", buckets);
+        updateEstimatedCompactionsByTasks(buckets);
+
+        List<List<SSTableReader>> prunedBuckets = new ArrayList<List<SSTableReader>>();
+        for (List<SSTableReader> bucket : buckets)
+        {
+            if (bucket.size() < cfs.getMinimumCompactionThreshold())
+                continue;
+
+            Collections.sort(bucket, new Comparator<SSTableReader>()
+            {
+                public int compare(SSTableReader o1, SSTableReader o2)
+                {
+                    return o1.descriptor.generation - o2.descriptor.generation;
+                }
+            });
+            prunedBuckets.add(bucket.subList(0, Math.min(bucket.size(), cfs.getMaximumCompactionThreshold())));
+        }
+
+        if (prunedBuckets.isEmpty())
+            return null;
+
+        List<SSTableReader> smallestBucket = Collections.min(prunedBuckets, new Comparator<List<SSTableReader>>()
+        {
+            public int compare(List<SSTableReader> o1, List<SSTableReader> o2)
+            {
+                long n = avgSize(o1) - avgSize(o2);
+                if (n < 0)
+                    return -1;
+                if (n > 0)
+                    return 1;
+                return 0;
+            }
+
+            private long avgSize(List<SSTableReader> sstables)
+            {
+                long n = 0;
+                for (SSTableReader sstable : sstables)
+                    n += sstable.bytesOnDisk();
+                return n / sstables.size();
+            }
+        });
+        return new CompactionTask(cfs, smallestBucket, gcBefore);
+    }
+
+    public AbstractCompactionTask getMaximalTask(final int gcBefore)
+    {
+        return cfs.getSSTables().isEmpty() ? null : new CompactionTask(cfs, filterSuspectSSTables(cfs.getSSTables()), gcBefore);
+    }
+
+    public AbstractCompactionTask getUserDefinedTask(Collection<SSTableReader> sstables, final int gcBefore)
+    {
+        return new CompactionTask(cfs, sstables, gcBefore)
+                .isUserDefined(true);
+    }
+
+    public int getEstimatedRemainingTasks()
+    {
+        return estimatedRemainingTasks;
+    }
+
+    private static List<Pair<SSTableReader, Long>> createSSTableAndLengthPairs(Collection<SSTableReader> collection)
+    {
+        List<Pair<SSTableReader, Long>> tableLengthPairs = new ArrayList<Pair<SSTableReader, Long>>(collection.size());
+        for(SSTableReader table: collection)
+            tableLengthPairs.add(new Pair<SSTableReader, Long>(table, table.onDiskLength()));
+        return tableLengthPairs;
+    }
+
+    /*
+     * Group files of similar size into buckets.
+     */
+    static <T> List<List<T>> getBuckets(Collection<Pair<T, Long>> files, long minSSTableSize)
+    {
+        // Sort the list in order to get deterministic results during the grouping below
+        List<Pair<T, Long>> sortedFiles = new ArrayList<Pair<T, Long>>(files);
+        Collections.sort(sortedFiles, new Comparator<Pair<T, Long>>()
+        {
+            public int compare(Pair<T, Long> p1, Pair<T, Long> p2)
+            {
+                return p1.right.compareTo(p2.right);
+            }
+        });
+
+        Map<Long, List<T>> buckets = new HashMap<Long, List<T>>();
+
+        outer:
+        for (Pair<T, Long> pair: sortedFiles)
+        {
+            long size = pair.right;
+
+            // look for a bucket containing similar-sized files:
+            // group in the same bucket if it's w/in 50% of the average for this bucket,
+            // or this file and the bucket are all considered "small" (less than `minSSTableSize`)
+            for (Entry<Long, List<T>> entry : buckets.entrySet())
+            {
+                List<T> bucket = entry.getValue();
+                long oldAverageSize = entry.getKey();
+                if ((size > (oldAverageSize / 2) && size < (3 * oldAverageSize) / 2)
+                    || (size < minSSTableSize && oldAverageSize < minSSTableSize))
+                {
+                    // remove and re-add under new new average size
+                    buckets.remove(oldAverageSize);
+                    long totalSize = bucket.size() * oldAverageSize;
+                    long newAverageSize = (totalSize + size) / (bucket.size() + 1);
+                    bucket.add(pair.left);
+                    buckets.put(newAverageSize, bucket);
+                    continue outer;
+                }
+            }
+
+            // no similar bucket found; put it in a new one
+            ArrayList<T> bucket = new ArrayList<T>();
+            bucket.add(pair.left);
+            buckets.put(size, bucket);
+        }
+
+        return new ArrayList<List<T>>(buckets.values());
+    }
+
+    private void updateEstimatedCompactionsByTasks(List<List<SSTableReader>> tasks)
+    {
+        int n = 0;
+        for (List<SSTableReader> bucket: tasks)
+        {
+            if (bucket.size() >= cfs.getMinimumCompactionThreshold())
+                n += Math.ceil((double)bucket.size() / cfs.getMaximumCompactionThreshold());
+        }
+        estimatedRemainingTasks = n;
+    }
+
+    public long getMinSSTableSize()
+    {
+        return minSSTableSize;
+    }
+
+    public long getMaxSSTableSize()
+    {
+        return Long.MAX_VALUE;
+    }
+
+    public boolean isKeyExistenceExpensive(Set<? extends SSTable> sstablesToIgnore)
+    {
+        return cfs.getSSTables().size() - sstablesToIgnore.size() > 20;
+    }
+
+    public String toString()
+    {
+        return String.format("SizeTieredCompactionStrategy[%s/%s]",
+            cfs.getMinimumCompactionThreshold(),
+            cfs.getMaximumCompactionThreshold());
+    }
+}
