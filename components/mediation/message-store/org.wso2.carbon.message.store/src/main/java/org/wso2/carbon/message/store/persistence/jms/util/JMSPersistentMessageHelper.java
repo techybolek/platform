e7f64b8bb7317a73fbf7ad1ea3ec8226da5e62fd
ishan@wso2.com <ishan@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-06-20 07:19:30 +0000

    Applying a fix for ESBJAVA-1985.
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@175723 a5903396-d722-0410-b921-86c7d4935375

--- components/mediation/message-store/org.wso2.carbon.message.store/src/main/java/org/wso2/carbon/message/store/persistence/jms/util/JMSPersistentMessageHelper.java
+++ components/mediation/message-store/org.wso2.carbon.message.store/src/main/java/org/wso2/carbon/message/store/persistence/jms/util/JMSPersistentMessageHelper.java
@@ -33,6 +33,7 @@ import org.apache.axis2.context.ServiceGroupContext;
 import org.apache.axis2.description.AxisOperation;
 import org.apache.axis2.description.AxisService;
 import org.apache.axis2.engine.AxisConfiguration;
+import org.apache.commons.io.IOUtils;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.synapse.MessageContext;
@@ -49,12 +50,20 @@ import org.wso2.carbon.message.store.persistence.jms.message.JMSPersistentSynaps
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Iterator;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class JMSPersistentMessageHelper {
 
+    private static final String ABSTRACT_MC_PROPERTIES = "ABSTRACT_MC_PROPERTIES";
+    private static final String JSON_STREAM = "JSON_STREAM";
     private SynapseEnvironment synapseEnvironment;
 
     private Log log = LogFactory.getLog(JMSPersistentMessage.class);
@@ -86,9 +95,9 @@ public class JMSPersistentMessageHelper {
             }
             msgCtx.setMessageID(jmsAxis2MessageContext.getMessageID());
             msgCtx.getOptions().setAction(jmsAxis2MessageContext.getAction());
-
-            
-            
+            msgCtx.setDoingREST(jmsAxis2MessageContext.isDoingPOX());
+            msgCtx.setDoingMTOM(jmsAxis2MessageContext.isDoingMTOM());
+            msgCtx.setDoingSwA(jmsAxis2MessageContext.isDoingSWA());
             if (jmsAxis2MessageContext.getService() != null) {
                 AxisService axisService =
                         axisConfiguration.getServiceForActivation(jmsAxis2MessageContext.getService());
@@ -140,12 +149,34 @@ public class JMSPersistentMessageHelper {
                 msgCtx.getOptions().setTo(new EndpointReference(jmsAxis2MessageContext.getToAddress().trim()));
             }
 
+            Object map = jmsAxis2MessageContext.getProperties().get(ABSTRACT_MC_PROPERTIES);
+            jmsAxis2MessageContext.getProperties().remove(ABSTRACT_MC_PROPERTIES);
             msgCtx.setProperties(jmsAxis2MessageContext.getProperties());
             msgCtx.setTransportIn(axisConfiguration.
                     getTransportIn(jmsAxis2MessageContext.getTransportInName()));
             msgCtx.setTransportOut(axisConfiguration.
                     getTransportOut(jmsAxis2MessageContext.getTransportOutName()));
 
+            Object headers = jmsAxis2MessageContext.getProperties()
+                    .get(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS);
+            if (headers instanceof Map) {
+                setTransportHeaders(msgCtx, (Map) headers);
+            }
+            if (map instanceof Map) {
+                Map<String, Object> abstractMCProperties = (Map) map;
+                Iterator<String> properties = abstractMCProperties.keySet().iterator();
+                while (properties.hasNext()) {
+                    String property = properties.next();
+                    Object value = abstractMCProperties.get(property);
+                    if (JSON_STREAM.equals(property)) {
+                        if (value instanceof byte[]) {
+                            byte[] jsonStreamArray = (byte[]) value;
+                            value = IOUtils.toBufferedInputStream(new ByteArrayInputStream(jsonStreamArray));
+                        }
+                    }
+                    msgCtx.setProperty(property, value);
+                }
+            }
             JMSPersistentSynapseMessage jmsSynpaseMessageContext
                     = message.getJmsPersistentSynapseMessage();
 
@@ -219,6 +250,7 @@ public class JMSPersistentMessageHelper {
             jmsAxis2MessageContext.setDoingMTOM(msgCtx.isDoingMTOM());
             jmsAxis2MessageContext.setDoingSWA(msgCtx.isDoingSwA());
 
+            // TODO: Is there a better way to persist the envelope without converting to a string?
             String soapEnvelope = msgCtx.getEnvelope().toString();
             jmsAxis2MessageContext.setSoapEnvelope(soapEnvelope);
             jmsAxis2MessageContext.setFLOW(msgCtx.getFLOW());
@@ -230,6 +262,37 @@ public class JMSPersistentMessageHelper {
                 jmsAxis2MessageContext.setTransportOutName(msgCtx.getTransportOut().getName());
             }
 
+            Iterator<String> abstractMCProperties = msgCtx.getPropertyNames();
+            Map<String, Object> copy = new HashMap<String, Object>(msgCtx.getProperties().size());
+            while (abstractMCProperties.hasNext()) {
+                String propertyName = abstractMCProperties.next();
+                Object propertyValue = msgCtx.getProperty(propertyName);
+                if (propertyValue instanceof String
+                        || propertyValue instanceof Boolean
+                        || propertyValue instanceof Integer
+                        || propertyValue instanceof Double
+                        || propertyValue instanceof Character) {
+                    copy.put(propertyName, propertyValue);
+                }
+                if (JSON_STREAM.equals(propertyName)) {
+                    if (propertyValue instanceof InputStream) {
+                        InputStream jsonStream = (InputStream) propertyValue;
+                        if (jsonStream != null) {
+                            try {
+                                byte[] array = IOUtils.toByteArray(jsonStream);
+                                copy.put(propertyName, array);
+                            } catch (IOException e) {
+                                log.error("Failed to store JSON message. "
+                                          + e.getLocalizedMessage());
+                            }
+                        }
+                    }
+                }
+            }
+            jmsAxis2MessageContext.addProperty(ABSTRACT_MC_PROPERTIES, copy);
+            Map<String, String> transportHeaders = getTransportHeaders(msgCtx);
+            jmsAxis2MessageContext.addProperty(
+                    org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS, transportHeaders);
             Iterator<String> it = msgCtx.getProperties().keySet().iterator();
 
             while (it.hasNext()) {
@@ -316,5 +379,68 @@ public class JMSPersistentMessageHelper {
 		return bytes;
 	}
 
+    private static final class Replace {
+        public String CHAR;
+        public String STRING;
 
+        public Replace(String c, String string) {
+            CHAR = c;
+            STRING = string;
+        }
+    }
+    // Replace Strings
+    private static final Replace RS_HYPHEN = new Replace("-", "__HYPHEN__");
+    private static final Replace RS_EQUAL = new Replace("=", "__EQUAL__");
+    private static final Replace RS_SLASH = new Replace("/", "__SLASH__");
+    private static final Replace RS_COMMA = new Replace(",", "__COMMA__");
+    private static final Replace RS_SPACE = new Replace(" ", "__SPACE__");
+    private static final Replace RS_COLON = new Replace(":", "__COLON__");
+    private static final Replace RS_SEMICOLON = new Replace(";", "__SEMICOLON__");
+
+    private Map<String, String> getTransportHeaders(org.apache.axis2.context.MessageContext messageContext) {
+        Object headers = messageContext.getProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS);
+        if (!(headers instanceof Map)) {
+            return Collections.emptyMap();
+        }
+        Map<String, String> httpHeaders = new TreeMap<String, String>();
+        Iterator<Map.Entry> i = ((Map) headers).entrySet().iterator();
+        while (i.hasNext()) {
+            Map.Entry headerStr = i.next();
+            String fieldName = (String) headerStr.getKey();
+            String fieldValue = (String) headerStr.getValue();
+            fieldName = fieldName.replaceAll(RS_HYPHEN.CHAR, RS_HYPHEN.STRING);
+            fieldValue = fieldValue.replaceAll(RS_HYPHEN.CHAR, RS_HYPHEN.STRING);
+            fieldValue = fieldValue.replaceAll(RS_EQUAL.CHAR, RS_EQUAL.STRING);
+            fieldValue = fieldValue.replaceAll(RS_SLASH.CHAR, RS_SLASH.STRING);
+            fieldValue = fieldValue.replaceAll(RS_COMMA.CHAR, RS_COMMA.STRING);
+            fieldValue = fieldValue.replaceAll(RS_SPACE.CHAR, RS_SPACE.STRING);
+            fieldValue = fieldValue.replaceAll(RS_COLON.CHAR, RS_COLON.STRING);
+            fieldValue = fieldValue.replaceAll(RS_SEMICOLON.CHAR, RS_SEMICOLON.STRING);
+            httpHeaders.put(fieldName, fieldValue);
+        }
+        return httpHeaders;
+    }
+
+    private void setTransportHeaders(org.apache.axis2.context.MessageContext msgCtx, Map headers) {
+        if (headers == null || msgCtx == null) {
+            return;
+        }
+        Map<String, String> httpHeaders = new TreeMap<String, String>();
+        Iterator<Map.Entry> i = headers.entrySet().iterator();
+        while (i.hasNext()) {
+            Map.Entry headerEntry = i.next();
+            String fieldName = (String) headerEntry.getKey();
+            fieldName = fieldName.replaceAll(RS_HYPHEN.STRING, RS_HYPHEN.CHAR);
+            String fieldValue = (String) headerEntry.getValue();
+            fieldValue = fieldValue.replaceAll(RS_HYPHEN.STRING, RS_HYPHEN.CHAR);
+            fieldValue = fieldValue.replaceAll(RS_EQUAL.STRING, RS_EQUAL.CHAR);
+            fieldValue = fieldValue.replaceAll(RS_SLASH.STRING, RS_SLASH.CHAR);
+            fieldValue = fieldValue.replaceAll(RS_COMMA.STRING, RS_COMMA.CHAR);
+            fieldValue = fieldValue.replaceAll(RS_SPACE.STRING, RS_SPACE.CHAR);
+            fieldValue = fieldValue.replaceAll(RS_COLON.STRING, RS_COLON.CHAR);
+            fieldValue = fieldValue.replaceAll(RS_SEMICOLON.STRING, RS_SEMICOLON.CHAR);
+            httpHeaders.put(fieldName, fieldValue);
+        }
+        msgCtx.setProperty(org.apache.axis2.context.MessageContext.TRANSPORT_HEADERS, httpHeaders);
+    }
 }
