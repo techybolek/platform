a0812a0eb44de97c40ec9563d1272bdf44707e24
dharshanaw@wso2.com <dharshanaw@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-07-09 02:21:19 +0000

    Adding custom server support fot embedded tomcat
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@177619 a5903396-d722-0410-b921-86c7d4935375

--- platform-integration/test-automation-framework/org.wso2.carbon.automation.core/src/main/java/org/wso2/carbon/automation/core/customservers/tomcat/TomcatServerManager.java
+++ platform-integration/test-automation-framework/org.wso2.carbon.automation.core/src/main/java/org/wso2/carbon/automation/core/customservers/tomcat/TomcatServerManager.java
@@ -19,6 +19,7 @@
 package org.wso2.carbon.automation.core.customservers.tomcat;
 
 import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
 import org.apache.catalina.loader.WebappLoader;
 import org.apache.catalina.startup.Tomcat;
 import org.apache.commons.logging.Log;
@@ -30,15 +31,27 @@ import org.springframework.web.context.support.AnnotationConfigWebApplicationCon
 import java.io.File;
 import java.io.IOException;
 
-public class TomcatServerManager {
+public class TomcatServerManager{
     private final static Log log = LogFactory.getLog(TomcatServerManager.class);
+    Tomcat tomcat;
+    boolean isRunning = false;
+    private Thread tomcatThread = null;
+    int tomcatPort;
+    String tomcatClass= null;
 
-    public void startJaxRsServer(String className) throws Exception {
+    public TomcatServerManager(String className,String server, int port)
+    {
+             this.tomcatPort =port;
+        this.tomcatClass = className;
+
+    }
+
+    public void startJaxRsServer() throws Exception {
         final File base = createBaseDirectory();
         log.info("Using base folder: " + base.getAbsolutePath());
 
-        final Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
+      tomcat = new Tomcat();
+        tomcat.setPort(tomcatPort);
         tomcat.setBaseDir(base.getAbsolutePath());
 
         Context context = tomcat.addContext("/", base.getAbsolutePath());
@@ -49,14 +62,33 @@ public class TomcatServerManager {
         context.setLoader(new WebappLoader(Thread.currentThread().getContextClassLoader()));
 
         context.addParameter("contextClass", AnnotationConfigWebApplicationContext.class.getName());
-        context.addParameter("contextConfigLocation", className);
+        context.addParameter("contextConfigLocation", tomcatClass);
 
         tomcat.start();
         tomcat.getServer().await();
     }
 
+    public synchronized  void startServer()
+    {
+        if(tomcatThread==null)
+        {
+            tomcatThread= new Thread(){
+                public void run() {
+                    try {
+                        startJaxRsServer();
+                    } catch (Exception e) {
+                        log.error("Tomcat server startup failed ");
+                    }
+                }
+            };
+            tomcatThread.start();
+            isRunning = true;
+        }
+    }
+
+
     private File createBaseDirectory() throws IOException {
-        final File base = File.createTempFile("jaxrs-tmp-", "", new File(this.getClass().getResource("jaxrs_temp").toString()));
+        final File base = File.createTempFile("jaxrs-tmp-", "", new File("/home/dharshana/wso2source/carbon/platform/trunk/platform-integration/test-automation-framework/org.wso2.carbon.automation.core/src/main/resources"));
 
         if (!base.delete()) {
             throw new IOException("Cannot (re)create base folder: " + base.getAbsolutePath());
@@ -66,5 +98,30 @@ public class TomcatServerManager {
             throw new IOException("Cannot create base folder: " + base.getAbsolutePath());
         }
         return base;
+    }
+
+    public void stop() throws LifecycleException {
+        if(!isRunning) {
+            //LOG.warn("Tomcat server is not running @ port={}", port);
+            return;
+        }
+
+       // if(isInfo) LOG.info("Stopping the Tomcat server");
+
+        tomcat.stop();
+        isRunning = false;
+    }
+
+    public boolean isRunning() {
+        return isRunning;
+    }
+
+    public void run() {
+        try {
+            startJaxRsServer();
+        } catch (Exception e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        //To change body of implemented methods use File | Settings | File Templates.
     }
 }
---
+++ platform-integration/test-automation-framework/org.wso2.carbon.automation.core/src/main/java/org/wso2/carbon/automation/core/customservers/tomcat/TomcatServerType.java
@@ -0,0 +1,8 @@
+package org.wso2.carbon.automation.core.customservers.tomcat;
+
+public enum TomcatServerType {
+    jaxrs,
+    jaxws,
+    simplewebapp,
+    servelet
+}
