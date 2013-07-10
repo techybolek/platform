15812ca6140dc0cdd3ca484f6238343c87438914
lalaji@wso2.com <lalaji@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-07-10 13:18:53 +0000

    Fixed APIMANAGER-1355
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@177832 a5903396-d722-0410-b921-86c7d4935375

--- dependencies/jaggery/0.9.0.ALPHA2-wso2v4/modules/i18n/scripts/i18n.js
+++ dependencies/jaggery/0.9.0.ALPHA2-wso2v4/modules/i18n/scripts/i18n.js
@@ -1,6 +1,8 @@
+
 (function () {
     var localeResourcesBasePath = '',
             localizations = {};
+    fallBackLocalizations = {};
     //initialization with the request and loads the correct localization resources
     this.init = function (req, localeResourcePath) {
         var locale = req.getLocale();
@@ -12,6 +14,7 @@
             //Check relevant locale file exists
             if (file.isExists()) {
                 localizations = require(localeResourcesBasePath + 'locale_' + locale + '.json');
+                fallBackLocalizations = require(localeResourcesBasePath + 'locale_default.json');
             } else {  //If not reading Strings from default English locale file.
                 localizations = require(localeResourcesBasePath + 'locale_en.json');
             }
@@ -20,14 +23,17 @@
         }
     };
 
-    var getLocalString = function (key, fallback) {
+    var getLocalString = function (key) {
         if (localizations[key]) {
             return localizations[key]
+        } else if(fallBackLocalizations) {
+            return fallBackLocalizations[key];
         } else {
-            return  key;
+            return key;
         }
 
     };
+
 
     this.localize = function (key, fallback) {
         var localized = getLocalString(key);
