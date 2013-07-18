545bc8ffffb78f68c0f8af52f2bc40ddb05c8153
suresh@wso2.com <suresh@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-07-18 10:35:09 +0000

    now the loging pages are configurable
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@178593 a5903396-d722-0410-b921-86c7d4935375

--- components/identity/org.wso2.carbon.identity.application.authentication.endpoint/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/OAuth2Servlet.java
+++ components/identity/org.wso2.carbon.identity.application.authentication.endpoint/src/main/java/org/wso2/carbon/identity/application/authentication/endpoint/OAuth2Servlet.java
@@ -18,7 +18,11 @@
 package org.wso2.carbon.identity.application.authentication.endpoint;
 
 import java.io.IOException;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
+import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -29,26 +33,81 @@ public class OAuth2Servlet extends HttpServlet {
 	/**
 	 * 
 	 */
-    private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
+
+	private static Map<String, String> pages = new HashMap<String, String>();
+
+	public void init(ServletConfig config) throws ServletException {
+		// load all of em
+		Enumeration initParams = config.getInitParameterNames();
+		while (initParams.hasMoreElements()) {
+			String paramName = (String) initParams.nextElement();
+			pages.put(paramName, config.getInitParameter(paramName));
+		}
+
+	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,
 	                                                                              IOException {
 
+		String loginPage = null;
+		String errorPage = null;
+		String consentPage = null;
+
+		// loading configured pages
+		String applicationName = request.getParameter("application");
+
+		if (applicationName != null) {
+			String loginPageParam = applicationName.trim() + "-LoginPage";
+			String errorPageParam = applicationName.trim() + "-ErrorPage";
+			String consentPageParam = applicationName.trim() + "-ConsentPage";
+
+			String value = pages.get(loginPageParam);
+			if (value != null) {
+				loginPage = value;
+			}
+			value = pages.get(errorPageParam);
+			if (value != null) {
+				errorPage = value;
+			}
+			value = pages.get(consentPageParam);
+			if (value != null) {
+				consentPage = value;
+			}
+		}
+
+		// setting the login page
 		if (request.getRequestURI().contains("/oauth2_login.do")) {
+			if (loginPage != null) {
+				response.sendRedirect(loginPage + "?" + request.getQueryString());
+			} else if (pages.get("Global-LoginPage") != null) {
+				response.sendRedirect(pages.get("Global-LoginPage") + "?" + request.getQueryString());
+			} else {
+				request.getRequestDispatcher("oauth2/oauth2_login.jsp").forward(request, response);
+			}
 
-            request.getRequestDispatcher("oauth2/oauth2_login.jsp").forward(request, response);
-
+		// setting the error page	
 		} else if (request.getRequestURI().contains("/oauth2_error.do")) {
+			if (errorPage != null) {
+				response.sendRedirect(errorPage + "?" + request.getQueryString());
+			} else if (pages.get("Global-ErrorPage") != null) {
+				response.sendRedirect(pages.get("Global-ErrorPage") + "?" + request.getQueryString());
+			} else {
+				request.getRequestDispatcher("oauth2/oauth2_error.jsp").forward(request, response);
+			}
 
-            request.getRequestDispatcher("oauth2/oauth2_error.jsp").forward(request, response);
-			
+		// setting the consent page	
 		} else if (request.getRequestURI().contains("/oauth2_consent.do")) {
-
-			request.getRequestDispatcher("oauth2/oauth2_consent.jsp").forward(request, response);
+			if (consentPage != null) {
+				response.sendRedirect(consentPage + "?" + request.getQueryString());
+			} else if (pages.get("Global-ConsentPage") != null) {
+				response.sendRedirect(pages.get("Global-ConsentPage") + "?" + request.getQueryString());
+			} else {
+				request.getRequestDispatcher("oauth2/oauth2_consent.jsp").forward(request, response);
+			}
 
 		} else {
-
-            request.getRequestDispatcher("oauth2/oauth2_error.jsp").forward(request, response);
+			request.getRequestDispatcher("oauth2/oauth2_error.jsp").forward(request, response);
 
 		}
 
