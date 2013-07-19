10e44936f525378ccfa1331d43a472730b84a198
dushan@wso2.com <dushan@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-07-19 11:08:36 +0000

    introduced generic config location for twitter connector
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@178713 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/mediation/mediation-library/connectors/org.wso2.carbon.mediation.library.connectors.twitter/src/main/java/org/wso2/carbon/mediation/library/connectors/twitter/TwitterConfigMediator.java
@@ -0,0 +1,58 @@
+/*
+ * Copyright (c) 2005-2010, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.mediation.library.connectors.twitter;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.synapse.MessageContext;
+import org.wso2.carbon.mediation.library.connectors.core.AbstractConnector;
+import org.wso2.carbon.mediation.library.connectors.core.ConnectException;
+
+public class TwitterConfigMediator extends AbstractConnector {
+
+	private static Log log = LogFactory.getLog(TwitterConfigMediator.class);
+
+	public static final String CONSUMER_KEY = "oauth.consumerKey";
+	public static final String CONSUMER_SECRET = "oauth.consumerSecret";
+	public static final String ACCESS_TOKEN = "oauth.accessToken";
+	public static final String ACCESS_TOKEN_SECRET = "oauth.accessTokenSecret";
+
+	@Override
+	public void connect(MessageContext messageContext) throws ConnectException {
+		try {
+			String consumerKey = TwitterMediatorUtils.lookupFunctionParam(messageContext,
+					CONSUMER_KEY);
+			String consumerSecret = TwitterMediatorUtils.lookupFunctionParam(
+					messageContext, CONSUMER_SECRET);
+			String accessToken = TwitterMediatorUtils.lookupFunctionParam(messageContext,
+					ACCESS_TOKEN);
+			String accessTokenSecret = TwitterMediatorUtils.lookupFunctionParam(
+					messageContext, ACCESS_TOKEN_SECRET);
+
+			TwitterMediatorUtils.storeLoginUser(messageContext, consumerKey,
+					consumerSecret, accessToken, accessTokenSecret);
+			if (log.isDebugEnabled()) {
+				log.info("login user status done");
+			}
+		} catch (Exception e) {
+			log.error("Failed to login user: " + e.getMessage(), e);
+			TwitterMediatorUtils.storeErrorResponseStatus(messageContext, e);
+		}
+	}
+}
