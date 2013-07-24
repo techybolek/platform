1ce20a738bd97d09bba615715950009fe3c06c63
johann@wso2.com <johann@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-07-24 14:46:42 +0000

    Committing patch provided by Darshana
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@178955 a5903396-d722-0410-b921-86c7d4935375

--- components/identity/org.wso2.carbon.identity.user.store.configuration.deployer/src/main/java/org/wso2/carbon/identity/user/store/configuration/deployer/UserStoreConfigurationDeployer.java
+++ components/identity/org.wso2.carbon.identity.user.store.configuration.deployer/src/main/java/org/wso2/carbon/identity/user/store/configuration/deployer/UserStoreConfigurationDeployer.java
@@ -24,15 +24,7 @@ import org.apache.axis2.deployment.repository.util.DeploymentFileData;
 import org.apache.axis2.engine.AxisConfiguration;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.context.CarbonContext;
-import org.wso2.carbon.identity.user.store.configuration.deployer.internal.UserStoreConfigComponent;
-import org.wso2.carbon.user.api.RealmConfiguration;
-import org.wso2.carbon.user.core.config.UserStoreConfigXMLProcessor;
-import org.wso2.carbon.user.core.service.RealmService;
-
-import java.io.File;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import org.wso2.carbon.user.core.common.UserStoreDeploymentManager;
 
 /**
  * This is to deploy a new User Store Management Configuration file dropped or created at repository/deployment/server/userstores
@@ -62,54 +54,13 @@ public class UserStoreConfigurationDeployer extends AbstractDeployer {
      *          for any errors
      */
     public void deploy(DeploymentFileData deploymentFileData) throws DeploymentException {
-        String path = deploymentFileData.getAbsolutePath();
-        UserStoreConfigXMLProcessor userStoreXMLProcessor = new UserStoreConfigXMLProcessor(path);
-        RealmConfiguration realmConfiguration;
-        File userMgtConfigFile = new File(path);
-        String modifiedTenantId = null;
+        UserStoreDeploymentManager userStoreDeploymentManager = new UserStoreDeploymentManager();
+        userStoreDeploymentManager.deploy(deploymentFileData.getAbsolutePath());
 
-        try {
-            realmConfiguration = userStoreXMLProcessor.buildUserStoreConfigurationFromFile();
-            RealmService realmService = UserStoreConfigComponent.getRealmService();
-
-            //tenant modified secondary user store configuration
-            if (path.contains("tenants")) {
-                Pattern p = Pattern.compile("-?\\d+");
-                Matcher m = p.matcher(path);
-                while (m.find()) {
-                    modifiedTenantId = m.group();
-                }
-                int tenantId = Integer.parseInt(modifiedTenantId);
-                realmConfiguration.setTenantId(tenantId);
-                setSecondaryUserStore(realmService.getTenantUserRealm(tenantId).getRealmConfiguration(), realmConfiguration);
-
-            } else {
-                //super tenant modified secondary user store configuration
-                setSecondaryUserStore(realmService.getBootstrapRealmConfiguration(), realmConfiguration);
-            }
-                log.info("Realm configuration of tenant:" + CarbonContext.getCurrentContext().getTenantId() + "  modified with " + path);
-
-        } catch (Exception ex) {
-            throw new DeploymentException("The deployment of " + userMgtConfigFile.getName() + " is not valid.", ex);
-        }
 
     }
 
-    /**
-     * Set secondary user store at the very end of chain
-     *
-     * @param parent : primary user store
-     * @param child : secondary user store
-     */
-    private void setSecondaryUserStore(RealmConfiguration parent, RealmConfiguration child) {
-
-        while (parent.getSecondaryRealmConfig() != null) {
-            parent = parent.getSecondaryRealmConfig();
-        }
-
-        parent.setSecondaryRealmConfig(child);
-    }
-
+    
     /**
      * Trigger un-deploying of a deployed file. Removes the deleted user store from chain
      *
@@ -118,46 +69,9 @@ public class UserStoreConfigurationDeployer extends AbstractDeployer {
      *          for any errors
      */
     public void undeploy(String fileName) throws DeploymentException {
-        String[] fileNames = fileName.split(File.separator);
-        String domainName = fileNames[fileNames.length - 1].replace(".xml", "").replace("_", ".");
+        UserStoreDeploymentManager userStoreDeploymentManager = new UserStoreDeploymentManager();
+        userStoreDeploymentManager.undeploy(fileName);
 
-        int tenantId;
-        RealmConfiguration secondary;
-        try {
-            tenantId = CarbonContext.getCurrentContext().getTenantId();
-            RealmConfiguration realmConfig = UserStoreConfigComponent.getRealmService().getTenantUserRealm(tenantId).getRealmConfiguration();
-
-            while (realmConfig.getSecondaryRealmConfig() != null) {
-                secondary = realmConfig.getSecondaryRealmConfig();
-                if (secondary.getUserStoreProperty("DomainName").equalsIgnoreCase(domainName)) {
-                    realmConfig.setSecondaryRealmConfig(secondary.getSecondaryRealmConfig());
-                    log.info("User store: " + domainName + " of tenant:" + tenantId + " is removed.");
-                    return;
-                } else {
-                    realmConfig = realmConfig.getSecondaryRealmConfig();
-                }
-            }
-
-
-        } catch (Exception ex) {
-            throw new DeploymentException("Error occurred at undeploying " + domainName + " from tenant:" + CarbonContext.getCurrentContext().getTenantId(), ex);
-        }
-
-    }
-
-    /**
-     * Unset secondary user store
-     *
-     * @param parent : primary user store
-     * @param child : secondary user store
-     */
-    private void unSetSecondaryUserStore(RealmConfiguration parent, RealmConfiguration child) {
-
-        while (parent.getSecondaryRealmConfig() != null) {
-            parent = parent.getSecondaryRealmConfig();
-        }
-
-        parent.setSecondaryRealmConfig(child);
     }
 
 
