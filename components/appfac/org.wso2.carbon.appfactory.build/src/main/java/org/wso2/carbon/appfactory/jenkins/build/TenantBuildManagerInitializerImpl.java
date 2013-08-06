6bc76db169763389f059525bb7436e6c211ec5d8
shamika@wso2.com <shamika@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-06 16:32:47 +0000

    Changes added to create tenant in jenkins when a new tenant is created in af
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@180044 a5903396-d722-0410-b921-86c7d4935375

--- components/appfac/org.wso2.carbon.appfactory.build/src/main/java/org/wso2/carbon/appfactory/jenkins/build/TenantBuildManagerInitializerImpl.java
+++ components/appfac/org.wso2.carbon.appfactory.build/src/main/java/org/wso2/carbon/appfactory/jenkins/build/TenantBuildManagerInitializerImpl.java
@@ -15,21 +15,61 @@
  */
 package org.wso2.carbon.appfactory.jenkins.build;
 
+import java.rmi.RemoteException;
+
+import org.apache.axis2.AxisFault;
+import org.apache.axis2.client.ServiceClient;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.wso2.carbon.appfactory.buildserver.teanant.mgt.stub.BuildServerManagementServiceBuildServerManagementExceptionException;
+import org.wso2.carbon.appfactory.buildserver.teanant.mgt.stub.BuildServerManagementServiceStub;
+import org.wso2.carbon.appfactory.common.AppFactoryConstants;
 import org.wso2.carbon.appfactory.core.TenantBuildManagerInitializer;
+import org.wso2.carbon.appfactory.jenkins.build.internal.ServiceContainer;
+import org.wso2.carbon.utils.CarbonUtils;
 
 /**
  * Default implementation of {@link TenantBuildManagerInitializer}
  */
-public class TenantBuildManagerInitializerImpl implements TenantBuildManagerInitializer {
-    private static final Log log = LogFactory.getLog(TenantBuildManagerInitializerImpl.class);
+public class TenantBuildManagerInitializerImpl implements
+		TenantBuildManagerInitializer {
+	private static final Log log = LogFactory
+			.getLog(TenantBuildManagerInitializerImpl.class);
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onTenantCreation(String tenantDomain, String usagePlan) {
-        log.info("**********************Initializing build manager for " + tenantDomain + " with " + usagePlan + " *************");
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public void onTenantCreation(String tenantDomain, String usagePlan) {
+		log.info("**********************Initializing build manager for "
+				+ tenantDomain + " with " + usagePlan + " *************");
+		try {
+			
+			String endPoint = ServiceContainer.getAppFactoryConfiguration().
+                    getFirstProperty(JenkinsCIConstants.BASE_URL_CONFIG_SELECTOR) + "/services/BuildServerManagementService";
+			
+			BuildServerManagementServiceStub buildServerMgr = new BuildServerManagementServiceStub(endPoint);
+			ServiceClient client = buildServerMgr._getServiceClient();
+			CarbonUtils.setBasicAccessSecurityHeaders(
+					ServiceContainer.getAppFactoryConfiguration()
+							.getFirstProperty(
+									AppFactoryConstants.SERVER_ADMIN_NAME),
+					ServiceContainer.getAppFactoryConfiguration()
+							.getFirstProperty(
+									AppFactoryConstants.SERVER_ADMIN_PASSWORD),
+					client);
+			buildServerMgr.createTenant(tenantDomain);
+
+		} catch (AxisFault e) {
+			String msg = "Problem occurred when creaing tenant in build server";
+			log.error(msg, e);
+		} catch (RemoteException e) {
+			String msg = "Problem occurred when creaing tenant in build server";
+			log.error(msg, e);
+		} catch (BuildServerManagementServiceBuildServerManagementExceptionException e) {
+			String msg = "Problem occurred when creaing tenant in build server";
+			log.error(msg, e);
+		}
+
+	}
 }
