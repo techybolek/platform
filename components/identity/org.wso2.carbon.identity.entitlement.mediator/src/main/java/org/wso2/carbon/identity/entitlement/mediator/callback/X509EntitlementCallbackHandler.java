a037c0866b15eb269f662030d31ccd4570f62298
johann@wso2.com <johann@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-06 15:52:29 +0000

    Fixing https://wso2.org/jira/browse/ESBJAVA-2390
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@180041 a5903396-d722-0410-b921-86c7d4935375

--- components/identity/org.wso2.carbon.identity.entitlement.mediator/src/main/java/org/wso2/carbon/identity/entitlement/mediator/callback/X509EntitlementCallbackHandler.java
+++ components/identity/org.wso2.carbon.identity.entitlement.mediator/src/main/java/org/wso2/carbon/identity/entitlement/mediator/callback/X509EntitlementCallbackHandler.java
@@ -37,8 +37,19 @@ public class X509EntitlementCallbackHandler extends EntitlementCallbackHandler {
         Axis2MessageContext axis2Msgcontext = null;
         axis2Msgcontext = (Axis2MessageContext) synCtx;
         msgContext = axis2Msgcontext.getAxis2MessageContext();
+        // For WS-Security
         X509Certificate cert = (X509Certificate) msgContext.getProperty("X509Certificate");
-        return cert.getSubjectDN().getName();
+        if(cert == null){
+            // For mutual authentication
+            Object sslCertObject = msgContext.getProperty("ssl.client.auth.cert.X509");
+            javax.security.cert.X509Certificate[] certs = (javax.security.cert.X509Certificate[]) sslCertObject;
+            if(certs != null && certs.length > 0) {
+                return certs[0].getSubjectDN().getName();
+            }
+        } else {
+            return cert.getSubjectDN().getName();
+        }
+        return null;
     }
 
     /*
@@ -53,9 +64,22 @@ public class X509EntitlementCallbackHandler extends EntitlementCallbackHandler {
         Axis2MessageContext axis2Msgcontext = null;
         axis2Msgcontext = (Axis2MessageContext) synCtx;
         msgContext = axis2Msgcontext.getAxis2MessageContext();
+        // For WS-Security
         X509Certificate cert = (X509Certificate) msgContext.getProperty("X509Certificate");
-        String issuer = cert.getIssuerDN().getName();
-        String signatureAlgo = cert.getSigAlgName();
-        return new String[] { "Issuer#" + issuer, "SignatureAlgo#" + signatureAlgo };
+        if(cert == null){
+            // For mutual authentication
+            Object sslCertObject = msgContext.getProperty("ssl.client.auth.cert.X509");
+            javax.security.cert.X509Certificate[] certs = (javax.security.cert.X509Certificate[]) sslCertObject;
+            if(certs != null && certs.length > 0) {
+                String issuer = certs[0].getIssuerDN().getName();
+                String signatureAlgo = certs[0].getSigAlgName();
+                return new String[] { "Issuer#" + issuer, "SignatureAlgo#" + signatureAlgo };
+            }
+        } else {
+            String issuer = cert.getIssuerDN().getName();
+            String signatureAlgo = cert.getSigAlgName();
+            return new String[] { "Issuer#" + issuer, "SignatureAlgo#" + signatureAlgo };
+        }
+        return null;
     }
 }
