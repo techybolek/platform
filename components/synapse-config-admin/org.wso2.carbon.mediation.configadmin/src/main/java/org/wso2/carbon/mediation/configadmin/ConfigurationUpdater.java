5589de5e36a57fa9b176dc210dd2d403b807463a
shafreen@wso2.com <shafreen@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-06 08:59:08 +0000

    Fixed to support the new implementation of MSMP
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@179946 a5903396-d722-0410-b921-86c7d4935375

--- components/synapse-config-admin/org.wso2.carbon.mediation.configadmin/src/main/java/org/wso2/carbon/mediation/configadmin/ConfigurationUpdater.java
+++ components/synapse-config-admin/org.wso2.carbon.mediation.configadmin/src/main/java/org/wso2/carbon/mediation/configadmin/ConfigurationUpdater.java
@@ -53,7 +53,7 @@ import org.apache.synapse.libraries.model.Library;
 import org.apache.synapse.libraries.util.LibDeployerUtils;
 import org.apache.synapse.mediators.base.SequenceMediator;
 import org.apache.synapse.mediators.template.TemplateMediator;
-import org.apache.synapse.message.processors.MessageProcessor;
+import org.apache.synapse.message.processor.MessageProcessor;
 import org.apache.synapse.message.store.MessageStore;
 import org.apache.synapse.rest.API;
 import org.apache.synapse.task.TaskDescriptionRepository;
