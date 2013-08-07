1a9a70d981c491e82e648a69bcce043b0fa5b083
malaka@wso2.com <malaka@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-07 06:08:42 +0000

    refactored code commit
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@180097 a5903396-d722-0410-b921-86c7d4935375

---
+++ components/mediation/mediation-library/connectors/org.wso2.carbon.connector.twilio/src/main/java/org/wso2/carbon/connector/twilio/usage/UpdateUsageTrigger.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright (c) 2005-2013, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.wso2.carbon.connector.twilio.usage;
+
+import org.apache.axiom.om.OMElement;
+import org.apache.synapse.MessageContext;
+import org.apache.synapse.SynapseException;
+import org.apache.synapse.SynapseLog;
+import org.wso2.carbon.connector.twilio.util.TwilioUtil;
+import org.wso2.carbon.mediation.library.connectors.core.AbstractConnector;
+import org.wso2.carbon.mediation.library.connectors.core.ConnectException;
+import org.wso2.carbon.mediation.library.connectors.core.util.ConnectorUtils;
+
+import com.twilio.sdk.TwilioRestClient;
+import com.twilio.sdk.TwilioRestResponse;
+import com.twilio.sdk.resource.instance.Account;
+
+/*
+ * Class mediator for getting a an USAGE triggers
+ * For more information, see http://www.twilio.com/docs/api/rest/usage-triggers
+ */
+public class UpdateUsageTrigger extends AbstractConnector {
+
+	public void connect(MessageContext messageContext) throws ConnectException {
+
+		SynapseLog log = getLog(messageContext);
+		log.auditLog("Start: get usage trigger");
+		String triggerSid =
+		                    (String) ConnectorUtils.lookupFunctionParam(messageContext,
+		                                                                TwilioUtil.PARAM_USAGE_TRIGGER_SID);
+
+		try {
+
+			TwilioRestClient twilioRestClient = TwilioUtil.getTwilioRestClient(messageContext);
+			twilioRestClient.get
+			OMElement omResponse = TwilioUtil.parseResponse("account.update.success");
+			TwilioUtil.addElement(omResponse, TwilioUtil.TWILIO_ACCOUNT_SID, account.getSid());
+			TwilioUtil.preparePayload(messageContext, omResponse);
+		} catch (Exception e) {
+			log.error(e.getMessage());
+			throw new SynapseException(e);
+		}
+		log.auditLog("End: get usage trigger");
+	}
+}
