8df1fd23cd9429f21e61a6651cb503eeb3bc6613
lalaji@wso2.com <lalaji@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-09 14:17:10 +0000

    Fixing APIMANAGER-1439
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@180505 a5903396-d722-0410-b921-86c7d4935375

--- components/apimgt/api-publisher-web/src/site/themes/default/templates/item-add/js/load.js
+++ components/apimgt/api-publisher-web/src/site/themes/default/templates/item-add/js/load.js
@@ -1,49 +1,61 @@
 function loadTiers(row) {
 
-    var target = document.getElementById("tier");
-
     jagg.post("/site/blocks/item-add/ajax/add.jag", { action:"getTiers" },
-        function (result) {
-            if (!result.error) {
-                var arr = [];
-                $('.getThrottlingTier',row).html('');
-                $('.postThrottlingTier',row).html('');
-                $('.putThrottlingTier',row).html('');
-                $('.deleteThrottlingTier',row).html('');
-                $('.optionsThrottlingTier',row).html('');
+              function (result) {
+                  if (!result.error) {
+                      var arr = [];
+                      $('.getThrottlingTier',row).html('');
+                      $('.postThrottlingTier',row).html('');
+                      $('.putThrottlingTier',row).html('');
+                      $('.deleteThrottlingTier',row).html('');
+                      $('.optionsThrottlingTier',row).html('');
 
-                for (var i = 0; i < result.tiers.length; i++) {
-                    arr.push(result.tiers[i].tierName);
-                    var k = result.tiers.length - i -1;
-                    $('.getThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
-                    $('.postThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
-                    $('.putThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
-                    $('.deleteThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
-                    $('.optionsThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
-                }
-                for (var j = 0; j < arr.length; j++) {
-                    option = new Option(arr[j], arr[j]);
-                    target.options[j] = option;
-                    target.options[j].title = result.tiers[j].tierDescription;
+                      for (var i = 0; i < result.tiers.length; i++) {
+                          var k = result.tiers.length - i -1;
+                          $('.getThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
+                          $('.postThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
+                          $('.putThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
+                          $('.deleteThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
+                          $('.optionsThrottlingTier',row).append($('<option value="'+result.tiers[k].tierName+'" title="'+result.tiers[k].tierDescription+'">'+result.tiers[k].tierName+'</option>'));
+                      }
 
-                    if (j == 0) {
-                        target.options[j].selected = 'selected';
-                        $("#tiersHelp").html(result.tiers[0].tierDescription);
-                        var tierArr = [];
-                        tierArr.push(target.options[j].value);
-                        $('<input>').attr('type', 'hidden')
-                            .attr('name', 'tiersCollection')
-                            .attr('id', 'tiersCollection')
-                            .attr('value', tierArr)
-                            .appendTo('#addAPIForm');
-                    }
-                }
-            }
-        }, "json");
+                  }
+              }, "json");
 }
 
 
+
 $(document).ready(function() {
+    var target = document.getElementById("tier");
+
+    jagg.post("/site/blocks/item-add/ajax/add.jag", { action:"getTiers" },
+              function (result) {
+                  if (!result.error) {
+                      var arr = [];
+
+                      for (var i = 0; i < result.tiers.length; i++) {
+                          arr.push(result.tiers[i].tierName);
+
+                      }
+                      for (var j = 0; j < arr.length; j++) {
+                          option = new Option(arr[j], arr[j]);
+                          target.options[j] = option;
+                          target.options[j].title = result.tiers[j].tierDescription;
+
+                          if (j == 0) {
+                              target.options[j].selected = 'selected';
+                              $("#tiersHelp").html(result.tiers[0].tierDescription);
+                              var tierArr = [];
+                              tierArr.push(target.options[j].value);
+                              $('<input>').attr('type', 'hidden')
+                                      .attr('name', 'tiersCollection')
+                                      .attr('id', 'tiersCollection')
+                                      .attr('value', tierArr)
+                                      .appendTo('#addAPIForm');
+                          }
+                      }
+                  }
+              }, "json");
 
     $('.transports_check_http').change(function(){
         if($(this).is(":checked")){
@@ -54,12 +66,12 @@ $(document).ready(function() {
     });
 
     $('.transports_check_https').change(function(){
-            if($(this).is(":checked")){
-                $(https_checked).val($(this).val());
-            }else{
-                $(https_checked).val("");
-            }
-        });
+        if($(this).is(":checked")){
+            $(https_checked).val($(this).val());
+        }else{
+            $(https_checked).val("");
+        }
+    });
 
     $("select[name='tier']").change(function() {
         // multipleValues will be an array
@@ -84,46 +96,46 @@ $(document).ready(function() {
     });
 
     var v = $("#addAPIForm").validate({
-        submitHandler: function(form) {
-            //Adding custom validation for the resource url UI
-            if(validateResourceTable() != ""){
-                return;
-            }
+                                          submitHandler: function(form) {
+                                              //Adding custom validation for the resource url UI
+                                              if(validateResourceTable() != ""){
+                                                  return;
+                                              }
 
 
-            $('#saveMessage').show();
-            $('#saveButtons').hide();
-            $(form).ajaxSubmit({
-                success:function(responseText, statusText, xhr, $form) {
-                    if (!responseText.error) {
-                        var current = window.location.pathname;
-                        if (current.indexOf(".jag") >= 0) {
-                            location.href = "index.jag";
-                        } else {
-                            location.href = 'site/pages/index.jag';
-                        }
-                    } else {
-                        if (responseText.message == "timeout") {
-                            if (ssoEnabled) {
-                                var currentLoc = window.location.pathname;
-                                if (currentLoc.indexOf(".jag") >= 0) {
-                                    location.href = "add.jag";
-                                } else {
-                                    location.href = 'site/pages/add.jag';
-                                }
-                            } else {
-                                jagg.showLogin();
-                            }
-                        } else {
-                            jagg.message({content:responseText.message,type:"error"});
-                        }
-                        $('#saveMessage').hide();
-                        $('#saveButtons').show();
-                    }
-                }, dataType: 'json'
-            });
-        }
-    });
+                                              $('#saveMessage').show();
+                                              $('#saveButtons').hide();
+                                              $(form).ajaxSubmit({
+                                                                     success:function(responseText, statusText, xhr, $form) {
+                                                                         if (!responseText.error) {
+                                                                             var current = window.location.pathname;
+                                                                             if (current.indexOf(".jag") >= 0) {
+                                                                                 location.href = "index.jag";
+                                                                             } else {
+                                                                                 location.href = 'site/pages/index.jag';
+                                                                             }
+                                                                         } else {
+                                                                             if (responseText.message == "timeout") {
+                                                                                 if (ssoEnabled) {
+                                                                                     var currentLoc = window.location.pathname;
+                                                                                     if (currentLoc.indexOf(".jag") >= 0) {
+                                                                                         location.href = "add.jag";
+                                                                                     } else {
+                                                                                         location.href = 'site/pages/add.jag';
+                                                                                     }
+                                                                                 } else {
+                                                                                     jagg.showLogin();
+                                                                                 }
+                                                                             } else {
+                                                                                 jagg.message({content:responseText.message,type:"error"});
+                                                                             }
+                                                                             $('#saveMessage').hide();
+                                                                             $('#saveButtons').show();
+                                                                         }
+                                                                     }, dataType: 'json'
+                                                                 });
+                                          }
+                                      });
 
 
 });
@@ -152,27 +164,27 @@ function getContextValue() {
 }
 
 function showHideRoles(){
-	var visibility = $('#visibility').find(":selected").val();
-	
-	if (visibility == "public" || visibility == "controlled"){
-		$('#rolesDiv').hide();
-	} else{
-		$('#rolesDiv').show();
-	}
-	if (visibility == "controlled") {
-		$('#allowTenantsDiv').show();
-	} else {
-		$('#allowTenantsDiv').hide();
-	}
+    var visibility = $('#visibility').find(":selected").val();
+
+    if (visibility == "public" || visibility == "controlled"){
+        $('#rolesDiv').hide();
+    } else{
+        $('#rolesDiv').show();
+    }
+    if (visibility == "controlled") {
+        $('#allowTenantsDiv').show();
+    } else {
+        $('#allowTenantsDiv').hide();
+    }
 }
 
 function showUTProductionURL(){
-	var endpointType = $('#endpointType').find(":selected").val();
-	if(endpointType == "secured"){
-		$('#credentials').show();
-	}
-	else{
-		$('#credentials').hide();
-	}
-	
+    var endpointType = $('#endpointType').find(":selected").val();
+    if(endpointType == "secured"){
+        $('#credentials').show();
+    }
+    else{
+        $('#credentials').hide();
+    }
+
 }
