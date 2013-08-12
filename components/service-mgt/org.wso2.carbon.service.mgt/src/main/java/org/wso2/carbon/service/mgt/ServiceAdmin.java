544171ca4304b6c3ef7e27ae12a8abd019dfa8a2
sameera@wso2.com <sameera@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-12 13:48:07 +0000

    Fixing CARBON-14280. Patch by Manoj
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@180745 a5903396-d722-0410-b921-86c7d4935375

--- components/service-mgt/org.wso2.carbon.service.mgt/src/main/java/org/wso2/carbon/service/mgt/ServiceAdmin.java
+++ components/service-mgt/org.wso2.carbon.service.mgt/src/main/java/org/wso2/carbon/service/mgt/ServiceAdmin.java
@@ -928,6 +928,16 @@ public class ServiceAdmin extends AbstractAdmin implements ServiceAdminMBean {
                }
 
            }
+            // If service was deployed by CApp we need to manualy undeploy the service when deleting service group
+            // since the artifact is not inside hot deployment directory
+           if (fileName.contains("carbonapps")) {
+               // if its a proxy deployed using CApp it can be removed form above check
+               if (axisConfig.getService(axisService.getName()) != null){
+                   axisConfig.removeService(axisService.getName());
+                   //adding log per service in order to notify user about the service's state when viewing logs.
+                   log.info("Undeploying Axis2 Service: " + axisService.getName());
+               }
+           }
 
 
 
