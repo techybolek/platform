0ca8417b90cddf3f9512638f0d85f169721b551a
shamika@wso2.com <shamika@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-08-18 15:37:16 +0000

    Changes identified by the code review (on 13/08/2013)
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@181854 a5903396-d722-0410-b921-86c7d4935375

--- products/appfactory/modules/webapps/jenkins-tenant-ext/src/main/java/org/wso2/carbon/appfactory/jenkinsext/AppfactoryPluginDeployer.java
+++ products/appfactory/modules/webapps/jenkins-tenant-ext/src/main/java/org/wso2/carbon/appfactory/jenkinsext/AppfactoryPluginDeployer.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2005-2011, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2005-2011, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.appfactory.jenkinsext;
 
 import java.io.File;
@@ -26,21 +26,27 @@ public class AppfactoryPluginDeployer {
 
 	/**
 	 * Deploys Jenkins plugins to given {@code dest}
+	 * 
 	 * @param src
 	 * @param dest
 	 * @throws IOException
+	 *             - if the error occurred while copying plugins to {@code dest}
+	 *             folder.
+	 *             IllegalArgumentException - invalid plugin source location is
+	 *             given.
 	 */
-	 protected void copyBundledPlugin(String src, String dest) throws IOException {
-		 
-		 String pluginPath = src + File.separator + JenkinsTenantConstants.COMMON_PLUGINS_DIR;
-		 File source = new File(pluginPath);
-		 
-		 if(!source.exists()){
-			 throw new IllegalArgumentException("Common plugin location cannot be found at " + pluginPath );
-		 }
-		 
-		 File destination = new File(dest, "plugins");
-		 FileUtils.copyDirectory(source, destination);
-	 }
-	
+	protected void copyBundledPlugin(String src, String dest) throws IOException {
+
+		String pluginPath = src + File.separator + JenkinsTenantConstants.COMMON_PLUGINS_DIR;
+		File source = new File(pluginPath);
+
+		if (!source.exists()) {
+			throw new IllegalArgumentException("Common plugin location cannot be found at " +
+			                                   pluginPath);
+		}
+
+		File destination = new File(dest, "plugins");
+		FileUtils.copyDirectory(source, destination);
+	}
+
 }
--- products/appfactory/modules/webapps/jenkins-tenant-ext/src/main/java/org/wso2/carbon/appfactory/jenkinsext/PostInitializer.java
+++ products/appfactory/modules/webapps/jenkins-tenant-ext/src/main/java/org/wso2/carbon/appfactory/jenkinsext/PostInitializer.java
@@ -1,20 +1,20 @@
 /*
-*  Copyright (c) 2005-2011, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
-*
-*  WSO2 Inc. licenses this file to you under the Apache License,
-*  Version 2.0 (the "License"); you may not use this file except
-*  in compliance with the License.
-*  You may obtain a copy of the License at
-*
-*    http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing,
-* software distributed under the License is distributed on an
-* "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
-* KIND, either express or implied.  See the License for the
-* specific language governing permissions and limitations
-* under the License.
-*/
+ * Copyright (c) 2005-2011, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ * 
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
 package org.wso2.carbon.appfactory.jenkinsext;
 
 import java.io.IOException;
@@ -29,42 +29,48 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
 /**
- * Class to initialize multi-tenanted Jenkins when the first request comes.  
+ * Class to initialize multi-tenanted Jenkins when the first request comes.
  */
-public class PostInitializer implements ServletContextListener{
+public class PostInitializer implements ServletContextListener {
 
 	private static final Log log = LogFactory.getLog(PostInitializer.class);
-	
+
 	public void contextInitialized(ServletContextEvent event) {
-		
-		log.debug("App factory post initilization invoked when initializing the context.");
-		log.debug("Context path - " + event.getServletContext().getContextPath());
-		
+
+		if (log.isDebugEnabled()) {
+			log.debug("App factory post initialization invoked when initializing the context.");
+			log.debug("Context path - " + event.getServletContext().getContextPath());
+		}
+
 		try {
 			String tenantHome = System.getenv(JenkinsTenantConstants.JENKINS_TENANT_HOME);
-			if(tenantHome == null){
-				String erroMsg = JenkinsTenantConstants.JENKINS_TENANT_HOME + " is not set or not pointed to valid location.";
+			if (tenantHome == null) {
+				String erroMsg =
+				                 JenkinsTenantConstants.JENKINS_TENANT_HOME +
+				                         " is not set or not pointed to valid location.";
 				log.fatal(erroMsg);
-				throw new NullPointerException(erroMsg);				
-			}else{
+				throw new NullPointerException(erroMsg);
+			} else {
 				log.info(JenkinsTenantConstants.JENKINS_TENANT_HOME + " is set to " + tenantHome);
 			}
-			
+
 			InitialContext iniCtxt = new InitialContext();
-            Context env = (Context) iniCtxt.lookup("java:comp/env");
-            String jenkinsHome = (String) env.lookup("JENKINS_HOME");
-			
-            AppfactoryPluginDeployer pluginDeloyer = new AppfactoryPluginDeployer();
+			Context env = (Context) iniCtxt.lookup("java:comp/env");
+			String jenkinsHome = (String) env.lookup("JENKINS_HOME");
+
+			AppfactoryPluginDeployer pluginDeloyer = new AppfactoryPluginDeployer();
 			pluginDeloyer.copyBundledPlugin(tenantHome, jenkinsHome);
-			 
-        } catch (IOException e) {
-	        e.printStackTrace();
-        }  catch (NamingException e) {
-        	e.printStackTrace();
-        }  
-    }
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (NamingException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public void contextDestroyed(ServletContextEvent event) {
-		log.debug("App factory post initilization invoked when detroying the context.");		
+		if (log.isDebugEnabled()) {
+			log.debug("App factory post initialization invoked when destroying the context.");
+		}
 	}
 }
