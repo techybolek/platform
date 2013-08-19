techybolek/platform

commit 5ef4ce507a30702747085a8c90d023818368e9ed

Fixing ESBJAVA-2318 and making HTTPEndpoint work in a thread safe manner.
git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@178674 a5903396-d722-0410-b921-86c7d4935375

dependencies/synapse/2.1.1-wso2v8/modules/core/src/main/java/org/apache/synapse/endpoints/HTTPEndpoint.java
@@ -28,6 +28,7 @@ import org.apache.synapse.rest.RESTConstants;
 import org.apache.synapse.util.xpath.SynapseXPath;
 import com.damnhandy.uri.template.UriTemplate;
 
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
@@ -42,7 +43,6 @@ public class HTTPEndpoint extends AbstractEndpoint {
 
     /*Todo*/
     /*Do we need HTTP Headers here?*/
-
 
     public void onFault(MessageContext synCtx) {
 
@@ -122,13 +122,23 @@ public class HTTPEndpoint extends AbstractEndpoint {
             }
         }
 
-        uriTemplate.set(variables);
+        // We have to create a local UriTemplate object, or else the UriTemplate.set(variables) call will fill up a list of variables and since uriTemplate
+        // is not thread safe, this list won't be clearing.
+        UriTemplate template = null;
+        template = UriTemplate.fromTemplate(uriTemplate.getTemplate());
+
+        if(template != null) {
+            template.set(variables);
+        }
+
         String evaluatedUri = "";
         try {
-            evaluatedUri = uriTemplate.expand();
+            evaluatedUri = new java.net.URI(template.expand()).getPath();
         } catch(ExpressionParseException e) {
             log.debug("No URI Template variables defined in HTTP Endpoint: " + this.getName());
-            evaluatedUri = uriTemplate.getTemplate();
+            evaluatedUri = template.getTemplate();
+        } catch (URISyntaxException e) {
+            evaluatedUri = template.getTemplate();
         }
         if (evaluatedUri != null) {
             synCtx.setTo(new EndpointReference(evaluatedUri));
@@ -136,8 +146,6 @@ public class HTTPEndpoint extends AbstractEndpoint {
                 super.getDefinition().setAddress(evaluatedUri);
             }
         }
-
-
     }
 
     public String getHttpMethod() {

commit 17a860708dbd87d524edda49ca74296e0428d23f

Fixing regression issues caused by r178674 and r178120 - merging branch fix r181920
git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@181921 a5903396-d722-0410-b921-86c7d4935375

dependencies/synapse/2.1.1-wso2v8/modules/core/src/main/java/org/apache/synapse/endpoints/HTTPEndpoint.java
@@ -126,25 +126,34 @@ public class HTTPEndpoint extends AbstractEndpoint {
             }
         }
 
-        uriTemplate.set(variables);
+        // We have to create a local UriTemplate object, or else the UriTemplate.set(variables) call will fill up a list of variables and since uriTemplate
+        // is not thread safe, this list won't be clearing.
+        UriTemplate template = null;
+        template = UriTemplate.fromTemplate(uriTemplate.getTemplate());
+
+        if(template != null) {
+            template.set(variables);
+        }
+
         String evaluatedUri = "";
         try {
-            evaluatedUri = uriTemplate.expand();
+            //URL url = new URL(URLDecoder.decode(template.expand(), "UTF-8"));
+            //evaluatedUri = url.toURI().toString();
+            evaluatedUri = template.expand();
             evaluatedUri=evaluatedUri.replace("%3A", ":");
             evaluatedUri=evaluatedUri.replace("%2F", "/");
             
         } catch(ExpressionParseException e) {
             log.debug("No URI Template variables defined in HTTP Endpoint: " + this.getName());
-            evaluatedUri = uriTemplate.getTemplate();
+            evaluatedUri = template.getTemplate();
         }
+
         if (evaluatedUri != null) {
             synCtx.setTo(new EndpointReference(evaluatedUri));
             if (super.getDefinition() != null) {
                 super.getDefinition().setAddress(evaluatedUri);
             }
         }
-
-
     }
 
     public String getHttpMethod() {

dependencies/synapse/2.1.1-wso2v8/modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughConstants.java
@@ -95,6 +95,9 @@ public class PassThroughConstants {
 
     // This is similar to isDoingREST  - if the request contains a REST (i.e. format=POX | GET | REST) call, then we set this to TRUE
     public static final String INVOKED_REST = "invokedREST";
+
+    // Use this to make PassThroughHttpSender set the Message Formatter's writeTo() preserve boolean value
+    public static final String FORMATTER_PRESERVE = "chunkedFormatterPreserve";     
     
     public static final String CLONE_PASS_THROUGH_PIPE_REQUEST = "clone_pass-through.pipe_connected";
     

dependencies/synapse/2.1.1-wso2v8/modules/transports/core/nhttp/src/main/java/org/apache/synapse/transport/passthru/PassThroughHttpSender.java
@@ -342,7 +342,12 @@ public class PassThroughHttpSender extends AbstractHandler implements TransportS
 					ByteArrayOutputStream _out = new ByteArrayOutputStream();
 					MessageFormatter formatter =  MessageFormatterDecoratorFactory.createMessageFormatterDecorator(msgContext);
 					OMOutputFormat format = PassThroughTransportUtils.getOMOutputFormat(msgContext);
-					formatter.writeTo(msgContext, format, _out, false);
+                    if(null == msgContext.getProperty(PassThroughConstants.FORMATTER_PRESERVE) || msgContext.isPropertyTrue(PassThroughConstants.FORMATTER_PRESERVE)) {
+                        formatter.writeTo(msgContext, format, _out, true);
+                    } else {
+                        formatter.writeTo(msgContext, format, _out, false);
+                    }
+
 					try {
 	                    long messageSize =setStreamAsTempData(formatter,msgContext,format);
 	                    msgContext.setProperty(PassThroughConstants.PASSTROUGH_MESSAGE_LENGTH,messageSize);