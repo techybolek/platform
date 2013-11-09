f9564a45a0c84ca24cde643e26c94580e3505023
lasantha@wso2.com <lasantha@wso2.com@a5903396-d722-0410-b921-86c7d4935375>
2013-11-09 22:42:17 +0000

    Deleting CEP components at top level dir which are now inside event-processing
    git-svn-id: https://svn.wso2.org/repos/wso2/carbon/platform/trunk@190586 a5903396-d722-0410-b921-86c7d4935375

--- components/event-builder/org.wso2.carbon.event.builder.core/src/main/java/org/wso2/carbon/event/builder/core/EventListener.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.event.builder.core;
-
-public interface EventListener {
-    public void onAddDefinition(Object definition);
-
-    public void onRemoveDefinition(Object definition);
-}
--- components/event-processor/org.wso2.carbon.event.processor.ui/src/main/java/org/wso2/carbon/event/processor/ui/UIConstants.java
+++
@@ -1,7 +0,0 @@
-package org.wso2.carbon.event.processor.ui;
-
-public interface UIConstants {
-    String SIDDHI_DISTRIBUTED_PROCESSING = "siddhi.enable.distributed.processing";
-    String SIDDHI_SNAPSHOT_INTERVAL = "siddhi.persistence.snapshot.time.interval.minutes";
-
-}
